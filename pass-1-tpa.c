e0a67f723036c9cf0329965f238e3d1c1d823aea
Farhath Sulthana <farhathsulthana2001@gmail.com>
2022-01-26 18:44:24 +0530

    updated

--- pass-1-tpa.c
+++ pass-1-tpa.c
@@ -10,19 +10,18 @@ void main () {
 
     int start_address, loc_ctr, pgm_len;
     
-    inp_pgm_fp = fopen ("input-pgm-p1.dat", "r");
-    intermediate_fp = fopen ("output-intmdt-file-p1.dat", "w");
-    sym_tbl_fp = fopen ("output-symb-tbl-p1.dat", "w");
-    len_fp = fopen ("output-pgm-len-p1.dat", "w");
+    inp_pgm_fp = fopen ("inp-pgm-p1.dat", "r");
+    intermediate_fp = fopen ("intmdt-file-op-p1.dat", "w");
+    sym_tbl_fp = fopen ("symb-tbl-op-p1.dat", "w");
+    len_fp = fopen ("pgm-len-op-p1.dat", "w");
 
-    fscanf (inp_pgm_fp, "%s\t%s\t%s", label, opcode, operand);
+    fscanf (inp_pgm_fp, "%s\t%s\t%X", label, opcode, &start_address);
     if (strcmp (opcode, "START") == 0) {
-        start_address = atoi (operand);
         loc_ctr = start_address;
-        fprintf (intermediate_fp, "-\t%s\t%s\t%s\n", label, opcode, operand);
+        fprintf (intermediate_fp, "-\t%s\t%s\t%X\n", label, opcode, start_address);
     }
     else {
-        loc_ctr = 0;
+        loc_ctr = 0x0;
         printf ("\nInvalid Entries detected!\n");
     }
 
@@ -30,18 +29,18 @@ void main () {
 
     while (!feof (inp_pgm_fp)) {
 
-        fprintf (intermediate_fp, "%d\t%s\t%s\t%s\n", loc_ctr, label, opcode, operand);
+        fprintf (intermediate_fp, "%X\t%s\t%s\t%s\n", loc_ctr, label, opcode, operand);
 
         if (strcmp (label, "-") != 0) {
-            fprintf (sym_tbl_fp, "%s\t%d\n", label, loc_ctr);
+            fprintf (sym_tbl_fp, "%s\t%X\n", label, loc_ctr);
         }
 
-        opcd_tbl_fp = fopen ("input-opcode-table-p1.dat", "r");
+        opcd_tbl_fp = fopen ("opcode-table-p1.dat", "r");
         fscanf (opcd_tbl_fp, "%s\t%s", op_code, hexacode);
 
         while (!feof (opcd_tbl_fp)) {
             if (strcmp (opcode, op_code) == 0) {
-                loc_ctr += 3;
+                loc_ctr += 0x3;
                 break;
             }
             fscanf (opcd_tbl_fp, "%s\t%s", op_code, hexacode);
@@ -49,20 +48,20 @@ void main () {
         fclose (opcd_tbl_fp);
 
         if (strcmp (opcode, "WORD") == 0) {
-            loc_ctr += 3;
+            loc_ctr += 0x3;
         }
         else if (strcmp (opcode, "RESW") == 0) {
-            loc_ctr += (3*atoi(operand));
+            loc_ctr += (0x3*atoi(operand));
         }
         else if (strcmp (opcode, "RESB") == 0) {
             loc_ctr += atoi (operand);
         }
         else if (strcmp (opcode, "BYTE") == 0) {
             if (operand[0] == 'X') {
-                loc_ctr++;
+                loc_ctr += 0x1;
             }
             else {
-                loc_ctr += (strlen (operand)-2);
+                loc_ctr += (strlen (operand)-0x2);
             }
         }
 
@@ -72,7 +71,7 @@ void main () {
     if (strcmp (opcode, "END") == 0) {
         fprintf (intermediate_fp, "-\t-\t%s\t%s\n", opcode, operand);
         pgm_len = loc_ctr - start_address;
-        fprintf (len_fp, "%d\n", pgm_len);
+        fprintf (len_fp, "%X\n", pgm_len);
     }
 
     fclose (inp_pgm_fp);
